2bf4aed86c72cc21fc0246b0432aa548c00a4a6a
Song Jingyi <jingyisong@hust.edu.cn>
2024-05-20 16:45:01 +0800

    style: 在 vuIn schema 中使用明文存储 trigger poc 的命令

--- poc_gen.py
+++ poc_gen.py
@@ -1,6 +1,8 @@
+import base64
+
 def gen_poc(trigger):
     poc = ""
     if "poc" in trigger:
         poc += f'RUN wget {trigger["poc"]}\n'
-    poc += f'RUN echo -n {trigger["guide"]} | base64 -d > trigger.sh\n'
+    poc += f'RUN echo -n {base64.b64encode(str.encode(trigger["guide"])).decode()} | base64 -d > trigger.sh\n'
     return poc
