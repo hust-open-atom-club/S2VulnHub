7a96e7a8743d78ada6c4c287ea371c8b73ea376e
Song Jingyi <jingyisong@hust.edu.cn>
2024-06-03 14:35:41 +0800

    feat: info and inspect tag

--- cli.py
+++ cli.py
@@ -1,7 +1,10 @@
 import argparse
 import json
+from pprint import pprint
 
 from gen import gen_reproduce, scan_version
+from inspect_gen import list_all_tags_for_remote_git_repo
+from info_gen import get_build_arch, get_depend, get_raw
 
 
 if __name__ == "__main__":
@@ -12,9 +15,10 @@ if __name__ == "__main__":
         dest="command", help="commands to run", required=True
     )
 
-    reproduce = subparsers.add_parser("reproduce", help="reproduce a vulnerability")
-    reproduce.add_argument("-C", "--CVE", help="the CVE to reproduce", required=True)
-
+    reproduce = subparsers.add_parser(
+        "reproduce", help="reproduce a vulnerability")
+    reproduce.add_argument(
+        "-C", "--CVE", help="the CVE to reproduce", required=True)
 
     scan = subparsers.add_parser("scan", help="scan the vulnerability version")
     scan.add_argument("-C", "--CVE", help="the CVE to scan", required=True)
@@ -27,6 +31,16 @@ if __name__ == "__main__":
         dest="tags",
         action="append",
     )
+
+    inspect = subparsers.add_parser("inspect", help="list app repo tags")
+    inspect.add_argument("-A", "--app", help="app name", required=True)
+
+    info = subparsers.add_parser("info", help="get build and depend info")
+    info.add_argument("-A", "--app", help="app name", required=True)
+    info.add_argument("--raw", action="store_true", help="get raw info")
+    info.add_argument("--building", action="store_true", help="get build info")
+    info.add_argument("--dependency", action="store_true",
+                      help="get dependency info")
 
     args = parser.parse_args()
     if args.command == "reproduce":
@@ -43,3 +57,17 @@ if __name__ == "__main__":
             scan_version(schema)
         else:
             scan_version(schema, args.tags)
+    elif args.command == "inspect":
+        with open(f"./apps/{args.app}.json", "r") as f:
+            schema = json.loads(f.read())
+        pprint(list_all_tags_for_remote_git_repo(
+            f'https://github.com/{schema["software"]["user"]}/{schema["software"]["repo"]}'))
+    elif args.command == "info":
+        if args.raw:
+            get_raw(args.app)
+        elif args.building:
+            get_build_arch(args.app)
+        elif args.dependency:
+            get_depend(args.app)
+        else:
+            get_raw(args.app)
---
+++ info_gen.py
@@ -0,0 +1,130 @@
+import requests
+import re
+
+
+def get_raw_arch(app_name):
+    resp = requests.get(
+        f"https://gitlab.archlinux.org/archlinux/packaging/packages/{app_name}/-/raw/main/PKGBUILD?ref_type=heads"
+    )
+    if resp.status_code == 200:
+        return resp.text
+    else:
+        return None
+
+
+def get_raw_fedora(app_name):
+    resp = requests.get(
+        f"https://src.fedoraproject.org/rpms/{app_name}/raw/rawhide/f/{app_name}.spec" 
+    )
+    if resp.status_code == 200:
+        return resp.text
+    else:
+        return None
+
+
+def get_raw(app_name):
+    arch_info = get_raw_arch(app_name)
+    if arch_info is not None:
+        print("Arch info raw:")
+        print(arch_info)
+    else:
+        print("No arch info, please use a different app name")
+    print("-----     -----     -----     -----     -----     -----")
+    fedora_info = get_raw_fedora(app_name)
+    if fedora_info is not None:
+        print("Fedora info raw:")
+        print(fedora_info)
+    else:
+        print("No fedora info, please use a different app name")
+
+
+def get_depend_arch(app_name):
+    raw = get_raw_arch(app_name)
+    if raw is None:
+        print("No arch info, please use a different app name")
+        return
+    matches = re.findall(r"depends=\(.*?\)", raw, re.DOTALL)
+
+    depends = []
+    for match in matches:
+        depend_list = re.findall(r"'([^']*)'", match, re.DOTALL)
+        depends.extend(depend_list)
+    depends = sorted(set(depends))
+    print("Arch info depend:")
+    print(depends)
+
+
+def get_depend_fedora(app_name):
+    raw = get_raw_fedora(app_name)
+    if raw is None:
+        print("No fedora info, please use a different app name")
+        return
+    matches = re.findall(r"^BuildRequires:\s+(.*)$", raw, re.MULTILINE)
+
+    depends = []
+    for match in matches:
+        depends.extend(match.split())
+    depends = sorted(set(depends))
+    print("Fedora info depend:")
+    print(depends)
+
+
+def get_depend(app_name):
+    get_depend_arch(app_name)
+    print("-----     -----     -----     -----     -----     -----")
+    get_depend_fedora(app_name)
+
+
+def get_build_arch(app_name):
+    raw = get_raw_arch(app_name)
+    if raw is None:
+        print("No arch info, please use a different app name")
+        return
+    start_marker = "\nbuild() {\n"
+    end_marker = "\n}\n"
+
+    start_index = raw.find(start_marker)
+    end_index = raw.find(end_marker, start_index)
+
+    if start_index != -1 and end_index != -1:
+        start_index += len(start_marker)
+        captured_lines = raw[start_index:end_index]
+        lines = captured_lines.split("\n")
+
+        for line in lines:
+            print(line)
+
+
+def get_cxx(app_name, path):
+    url = f'https://api.github.com/repos/{app_name}/contents/{path if path is not None else ""}'
+
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        print("ERROR: No such repo or path, please check your input")
+        return
+
+    resp = resp.json()
+    for item in resp:
+        if "makefile" in item["name"].lower():
+            print("Makefile is detected!")
+        elif "cmake" in item["name"].lower():
+            print("CMake is detected!")
+        elif "meson" in item["name"].lower():
+            print("Meson is detected!")
+        elif "configure" in item["name"].lower():
+            print("Autotools is detected!")
+        elif "bazel" in item["name"].lower():
+            print("Bazel is detected!")
+        elif ".gn" in item["name"].lower():
+            print("GN is detected!")
+        elif "conan" in item["name"].lower():
+            print("Conan is detected!")
+        elif "scons" in item["name"].lower():
+            print("Scons is detected!")
+        elif "buck" in item["name"].lower():
+            print("Buck is detected!")
+        elif (
+            "moz.build" in item["name"].lower()
+            or "moz.configure" in item["name"].lower()
+        ):
+            print("mozbuild is detected!")
