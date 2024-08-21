5b20700e9db8bf13503e7702af75090c8ad02f74
Jingyi Song <jingyisong@hust.edu.cn>
2024-08-21 15:57:27 +0800

    style: 修改参数介绍，将 inspect tags 功能合并到 info 子命令

--- src/cli.py
+++ src/cli.py
@@ -14,30 +14,37 @@ if __name__ == "__main__":
         dest="command", help="commands to run", required=True
     )
 
-    reproduce = subparsers.add_parser("reproduce", help="reproduce a vulnerability")
-    reproduce.add_argument("-C", "--CVE", help="the CVE to reproduce", required=True)
+    # subcommand reproduce
+    reproduce = subparsers.add_parser(
+        "reproduce", help="generate Dockerfile for a vulnerability"
+    )
+    reproduce.add_argument("CVE", help="the CVE to generate Dockerfile")
 
-    scan = subparsers.add_parser("scan", help="scan the vulnerability version")
-    scan.add_argument("-C", "--CVE", help="the CVE to scan", required=True)
+    # subcommand scan
+    scan = subparsers.add_parser("scan", help="scan if the app version is vulnerable")
+    scan.add_argument("CVE", help="the CVE to scan")
     # customizing the scan target
     # eg. some distro only support some versions
     scan.add_argument(
         "-t",
         help="specify the tag to scan, None means scan all available tags",
         default=None,
-        dest="tags",
+        dest="target_tags",
         action="append",
     )
 
-    inspect = subparsers.add_parser("inspect", help="list app repo tags")
-    inspect.add_argument("-A", "--app", help="app name", required=True)
-
-    info = subparsers.add_parser("info", help="get build and depend info")
-    info.add_argument("-A", "--app", help="app name", required=True)
+    # subcommand info
+    info = subparsers.add_parser(
+        "info",
+        help="get software build and depend info, print raw info if no option is specified",
+    )
+    info.add_argument("app", help="app name")
     info.add_argument("--raw", action="store_true", help="get raw info")
     info.add_argument("--building", action="store_true", help="get build info")
     info.add_argument("--dependency", action="store_true", help="get dependency info")
+    info.add_argument("--tags", action="store_true", help="get all app repo tags")
 
+    # parse the args
     args = parser.parse_args()
     if args.command == "reproduce":
         with open(f"../data/user_cve/{args.CVE}.json", "r") as f:
@@ -48,19 +55,10 @@ if __name__ == "__main__":
     elif args.command == "scan":
         with open(f"../data/user_cve/{args.CVE}.json", "r") as f:
             schema = json.loads(f.read())
-
-        if args.tags is None:
+        if args.target_tags is None:
             scan_version(schema)
         else:
-            scan_version(schema, args.tags)
-    elif args.command == "inspect":
-        with open(f"../data/apps/{args.app}.json", "r") as f:
-            schema = json.loads(f.read())
-        pprint(
-            list_all_tags_for_remote_git_repo(
-                f'https://github.com/{schema["software"]["user"]}/{schema["software"]["repo"]}'
-            )
-        )
+            scan_version(schema, args.target_tags)
     elif args.command == "info":
         if args.raw:
             get_raw(args.app)
@@ -68,5 +66,13 @@ if __name__ == "__main__":
             get_build_arch(args.app)
         elif args.dependency:
             get_depend(args.app)
+        elif args.tags:
+            with open(f"../data/apps/{args.app}.json", "r") as f:
+                schema = json.loads(f.read())
+            pprint(
+                list_all_tags_for_remote_git_repo(
+                    f'https://github.com/{schema["software"]["user"]}/{schema["software"]["repo"]}'
+                )
+            )
         else:
             get_raw(args.app)
