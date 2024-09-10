07dbc0dbd1c95647ccc3acbc97b95f1f210cb171
Jingyi Song <jingyisong@hust.edu.cn>
2024-09-10 14:28:50 +0800

    doc: add docstring

--- src/soft_gen.py
+++ src/soft_gen.py
@@ -4,6 +4,13 @@ from urllib.parse import urlparse
 
 
 def extract_name_and_ext(url: str) -> Tuple[str, str]:
+    """
+    extract filename and extension from url
+
+    https://github.com/mudongliang/source-packages/raw/master/CVE-2017-5854/podofo-0.9.4.tar.gz
+
+    -> [podofo-0.9.4.tar.gz, .tar.gz]
+    """
     parsed_url = urlparse(url)
     filename = os.path.basename(parsed_url.path)
     _, ext = os.path.splitext(filename)
@@ -14,7 +21,19 @@ def extract_name_and_ext(url: str) -> Tuple[str, str]:
     return filename, ext
 
 
-def gen_soft(software, version=None):
+def gen_soft(software: dict, version: str = None) -> str:
+    """
+    generate vulnerable software dockerfile snippet
+
+    set workdir and checkout to the vulnerable commit id if version is not None
+
+    Args:
+        software (dict): software schema
+        version (str, optional): vulnerable software commit id. Defaults to None.
+
+    Returns:
+        str: software dockerfile snippet
+    """
     soft = ""
     source = software["source"]
     if source == "github":
